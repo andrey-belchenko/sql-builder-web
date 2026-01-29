1b52549e72304769b07ccd3d1d38e4dae34a2f5e
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-01-30 02:36:20 +0300

    dependancies

--- net/SqlBuilderApp/Program.cs
+++ net/SqlBuilderApp/Program.cs
@@ -11,7 +11,19 @@ namespace SqlBuilderApp
             // sql.builder.Program.TestReportsAnalysis(args);
             // sql.builder.Program.TestSqlParsing(args);
             //sql.builder.Program.Main2(args);
-            AnalyzerStorage.SaveAllCollectionsToFiles();
+            // AnalyzerStorage.SaveAllCollectionsToFiles();
+
+            string query = @"
+        SELECT object_name, object_type, processed
+        FROM report_dev_sqlb.db_objects
+        WHERE processed = false
+            AND object_name IN (
+                SELECT DISTINCT used_object_name
+                FROM report_dev_sqlb.report_dependencies
+                WHERE nav_id = 'nav310'
+            )";
+
+            DbObjectDependencyLoader.LoadDependencies(query);
         }
     }
 }
