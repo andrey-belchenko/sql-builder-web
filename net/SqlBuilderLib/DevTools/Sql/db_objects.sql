a2628bb10ea1f3cab8adb06fa0f4fa96202ca6a6
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-01-30 01:36:41 +0300

    dependancies

--- net/SqlBuilderLib/DevTools/Sql/db_objects.sql
+++ net/SqlBuilderLib/DevTools/Sql/db_objects.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS report_dev_sqlb.db_objects (
 );
 
 
-insert into  report_dev_sqlb.db_objects (object_name, object_type)
-select distinct used_object_name ,used_object_type from report_dev_sqlb.dependencies d;
+-- migrate
+-- insert into  report_dev_sqlb.db_objects (object_name, object_type)
+-- select distinct used_object_name ,used_object_type from report_dev_sqlb.dependencies d;
