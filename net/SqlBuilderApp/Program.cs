112f4e8722697d0676135049e37ab77282918965
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-01-30 03:23:20 +0300

    dependancies

--- net/SqlBuilderApp/Program.cs
+++ net/SqlBuilderApp/Program.cs
@@ -11,32 +11,32 @@ namespace SqlBuilderApp
             // sql.builder.Program.TestReportsAnalysis(args);
             // sql.builder.Program.TestSqlParsing(args);
             //sql.builder.Program.Main2(args);
-            // AnalyzerStorage.SaveAllCollectionsToFiles();
+            AnalyzerStorage.SaveAllCollectionsToFiles();
 
-            DevAnalyzer.SetConnectionAndSourceFolder();
-            string query = @"
-       select
-	*
-from
-	report_dev_sqlb.db_objects
-where
-	processed = false
-	and
-	object_name in (
-		select
-			distinct used_object_name
-		from
-			report_dev_sqlb.report_dependencies
-		where
-			nav_id = 'nav310'
-	)
---and object_name='sqlb_rep_61880_10.fill_table'
+//             DevAnalyzer.SetConnectionAndSourceFolder();
+//             string query = @"
+//        select
+// 	*
+// from
+// 	report_dev_sqlb.db_objects
+// where
+// 	processed = false
+// 	and
+// 	object_name in (
+// 		select
+// 			distinct used_object_name
+// 		from
+// 			report_dev_sqlb.report_dependencies
+// 		where
+// 			nav_id = 'nav310'
+// 	)
+// --and object_name='sqlb_rep_61880_10.fill_table'
 
-and object_name not in ('RAISE_APPLICATION_ERROR')
+// and object_name not in ('RAISE_APPLICATION_ERROR')
 
-";
+// ";
 
-            DbObjectDependencyLoader.LoadDependencies(query);
+//             DbObjectDependencyLoader.LoadDependencies(query);
         }
     }
 }
