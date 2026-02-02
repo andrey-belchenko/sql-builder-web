60c53a8c136e593d51c47d8c3aaf9886eb21a407
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-02 12:03:18 +0300

    dependanciesrefactor devtools

--- net/SqlBuilderApp/Program.cs
+++ net/SqlBuilderApp/Program.cs
@@ -8,9 +8,9 @@ namespace SqlBuilderApp
     {
         static void Main(string[] args)
         {
-            // sql.builder.Program.TestReportsAnalysis(args);
-            // sql.builder.Program.TestSqlParsing(args);
-            //sql.builder.Program.Main2(args);
+            // DevUtils.TestReportsAnalysis(args);
+            DevUtils.TestSqlParsing(args);
+            // sql.builder.Program.Main2(args);
             //             AnalyzerStorage.SaveAllCollectionsToFiles();
 
             //             DevAnalyzer.SetConnectionAndSourceFolder();
@@ -39,7 +39,7 @@ namespace SqlBuilderApp
             //             DbObjectDependencyLoader.LoadDependencies(query);
 
 
-            LogsLoader.LoadLogsFromCsvFiles();
+            // LogsLoader.LoadLogsFromCsvFiles();
         }
     }
 }
