7597a1d7df08bbdb47edf508bbd2a261e9651652
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-02 22:23:35 +0300

    routes

--- net/Asuse.Ai.Reports/Controllers/SqlBuilderController.cs
+++ net/Asuse.Ai.Reports/Controllers/SqlBuilderController.cs
@@ -14,10 +14,18 @@ namespace Asuse.Ai.Reports.Controllers
         }
 
         [HttpGet]
-        [Route("form")]
-        public IActionResult GetForm()
+        [Route("form-config/{id}")]
+        public IActionResult GetForm(string id)
         {
-            return Ok(new { message = "Hello from SqlBuilderController", timestamp = DateTime.UtcNow });
+            var dummyXml = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<form name=""{id}"">
+    <fields>
+        <field name=""field1"" type=""text"" label=""Field 1"" />
+        <field name=""field2"" type=""number"" label=""Field 2"" />
+    </fields>
+</form>";
+
+            return Content(dummyXml, "application/xml");
         }
 
     }
