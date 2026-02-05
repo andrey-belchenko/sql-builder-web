ed1058338bcf4c3eb02a6aa2982250ab9301034e
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-05 19:06:42 +0300

    build TS forms

---
+++ net/SqlBuilder.DevTools/TsBuilder.Field.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using Npgsql;
+using sql.builder;
+using sql.builder.Clean;
+using sql.builder.Clean.Extensions;
+using sql.builder.DataApi;
+
+namespace SqlBuilderLib.DevTools
+{
+    public static partial class TsBuilder
+    {
+        private static string ProcessField(VForm form, VField field)
+        {
+
+           return null;
+
+        }
+
+
+       
+    }
+}
