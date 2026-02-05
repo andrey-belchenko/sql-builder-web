2f142114f14c0f338704a35103ae374be6cc9933
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-05 21:01:10 +0300

    build TS forms

--- net/SqlBuilder.DevTools/TsBuilder.Query.cs
+++ net/SqlBuilder.DevTools/TsBuilder.Query.cs
@@ -1,4 +1,7 @@
 
+using System;
+using System.IO;
+using System.Text;
 using sql.builder.DataApi;
 
 namespace SqlBuilderLib.DevTools
@@ -9,11 +12,21 @@ namespace SqlBuilderLib.DevTools
         {
             if (queryCall == null) return;
 
-            var queryFileName = $"query_{ClearName(queryCall.P_Name)}.sql";
+            var fileName = $"query_{ClearName(queryCall.P_Name)}.sql";
+            var folderPath = Path.Combine(BasePath, "queries");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var filePath = Path.Combine(BasePath, "queries", fileName);
             var query = queryCall.Query();
             var sql = query.GetSql();
 
-
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, sql, Encoding.UTF8);
+                Console.WriteLine($"Extracted query: {filePath}");
+            }
 
         }
 
