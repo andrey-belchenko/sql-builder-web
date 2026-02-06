da53768fafe595a87b84c4f14fffde68962386ce
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-06 13:41:18 +0300

    build TS forms

--- net/SqlBuilder.DevTools/TsBuilder.Field.cs
+++ net/SqlBuilder.DevTools/TsBuilder.Field.cs
@@ -1,17 +1,71 @@
 
+using System.Collections.Generic;
 using sql.builder.DataApi;
+
+
+
+// Apply
+// "title"
+// "name"
+// "editable"
+// "column-editable"
+// "default"
+// "valid"
+// "visible"
+// "column-visible"
+// "mandatory"
+// "column-mandatory"
+// "valuequery"
+// "controlType"
+// "val-field-name"
+// "rows-limit"
+
+
+// Not implemented
+// "hint"
+// "format"
+// "step"
+// "search-field-name"
+// "expand-all"
+// "name-field-name"
+// "parent-field-name"
+// "edit-mask"
+
+// Incompatible
+// "special-type"
+// "clear-on-list-change"
+// "width-perc"
+// "halign"
+// "checked"
+// "show-checkbox"
+// "auto-check"
+// "type"
+// "show-nulls"
+
+
+// Unnecessary
+// "part-id"
+// "comment"
+// "class"
+// "field"
+// "id"
+
 
 namespace SqlBuilderLib.DevTools
 {
     public static partial class TsBuilder
     {
+        public static HashSet<string> attrNames =  new HashSet<string>();
         private static string ProcessField(VForm form, VField field)
         {
-
             var fieldName = field.P_Field;
-            var fieldFileName = $"field_{ClearName(fieldName)}";
+            var fieldFileName = $"field_{ClearName(fieldName)}.ts";
             ProcessQuery(field.ListQuery());
             ProcessQuery(field.DefaultQuery());
+
+            foreach (var attr in field.Attributes()){
+               attrNames.Add(attr.Name.LocalName); 
+            }
             return null;
 
         }
