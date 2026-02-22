34ed68e9d772d904e2d13b13c6eb467c7ebb5367
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-22 07:11:17 +0300

    ora2pg

--- oracle-to-pg/hepler-sql/find-reports.sql
+++ oracle-to-pg/hepler-sql/find-reports.sql
@@ -1,24 +1,43 @@
-with stat as (
-    select relname as table_name,
-        max(reltuples::bigint) as estimate_count
-    from pg_class c
-        join pg_namespace n on n.oid = c .relnamespace
-    where c .relkind = 'r'
-        and n.nspname not in ('pg_catalog', 'information_schema')
-    group by relname
+with stat as 
+
+(SELECT 
+    relname AS table_name, 
+    max(reltuples::bigint) AS estimate_count
+FROM 
+    pg_class c
+JOIN 
+    pg_namespace n ON n.oid = c.relnamespace
+WHERE 
+    c.relkind = 'r' 
+    AND n.nspname NOT IN ('pg_catalog', 'information_schema')
+    
+group by relname
 ),
-rs as (
-    select rd. *,
-        s.estimate_count cnt
-    from report_dev_sqlb.report_dependencies rd
-        left join stat s on s.table_name = rd.used_object_name
-    where used_object_type = 'table'
+    
+    rs as (
+
+select rd.* , s.estimate_count cnt
+
+from report_dev_sqlb.report_dependencies rd 
+left join  stat s on s.table_name =  rd.used_object_name
+
+where used_object_type='table')
+
+
+select * from  rs where 
+
+not exists (select * from rs rs1 where rs1.rep_name=rs.rep_name 
+and coalesce(cnt,0) =0
+--and cnt is  null
 )
-select *
-from rs
-where not exists (
-        select *
-        from rs rs1
-        where rs1.rep_name = rs.rep_name
-            and coalesce(cnt, 0) = 0 --and cnt is  null
-    );
+
+and exists (select * from  report_dev_sqlb.report_dependencies rd  where rd.rep_name=rs.rep_name 
+and  rd.used_object_type = 'procedure' 
+--and cnt is  null
+)
+
+
+and exists (
+
+select * from report_dev_sqlb.report_exec_stat s where s.name= rs.rep_name  and s.exec_3m>1
+)
